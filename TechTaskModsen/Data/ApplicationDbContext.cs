1a0af0f7242f9d10dffd5671a2e95dcb7f46d4c7
Filchenko Nikita <filchenko.niki@mail.ru>
2024-04-10 11:32:04 +0300

    feat: add db contex

---
+++ TechTaskModsen/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Collections.Generic;
+
+namespace TechTaskModsen.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
